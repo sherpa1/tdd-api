a1bd6a03087e2da245b5729df12f01c24b9eec05
sherpa1 <alex@sherpa.one>
2022-03-08 12:39:40 +0100

    check read tasks by id, and filtered by completed attr

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ app.use(express.json());//to access req.body
 
 const tasks = [];
 
-let i = 0;
+let i = 1;
 
 function random_boolean(){
     return Math.round(Math.random());
 }
 
-while(i<100){
-    tasks.push({id:i+1, content:faker.lorem.sentence(), completed:random_boolean()});
+while(i<101){
+    tasks.push({id:i, content:faker.lorem.sentence(), completed:random_boolean()});
     i++;
 }
 
@@ -25,7 +25,35 @@ app.get('/', (req,res,next)=>{
 //TODO:CRUD routes for Task Resource
 
 app.get('/tasks', (req,res,next)=>{
-    res.json(tasks);
+
+    if(req.query.completed){
+
+        const {completed} = req.query;
+
+        if(completed !== true && completed !== false && completed !== 1 && completed !== 0 && completed!=="0" && completed!=="1" && completed!=="true" && completed!=="false") res.sendStatus(400);
+
+        const filtered_tasks = tasks.filter(element => element.completed === Number(completed));
+        
+        res.json(filtered_tasks);
+    
+    }else{
+        res.json(tasks);
+    }
+
+});
+
+app.get('/tasks/:id', (req,res,next)=>{
+
+    const {id} = req.params;
+
+    const found_task = tasks.find(element => element.id === Number(id));
+
+    if(found_task){
+        res.json(found_task);
+    }else{
+        res.sendStatus(404);
+    }
+
 });
 
 app.listen(port, () => {
--- tests/api.test.js
+++ tests/api.test.js
@@ -8,3 +8,36 @@ describe('Read', ()=>{
         expect(response.status).toBe(200);
     });
 });
+
+describe('Read one task by id', ()=>{
+    
+    const id = 7;
+    
+    it('should return a task with id = 1 if id 7 exists', async ()=>{
+        const response = await axios.get(api_url+'/tasks/'+id);
+
+        expect(response.data).toEqual(
+            expect.objectContaining({id:7})
+        );
+    });
+});
+
+describe('Read task where completed = 1', ()=>{
+    
+    it('should return all tasks with completed attr = 1', async ()=>{
+        const response = await axios.get(api_url+'/tasks?completed=1');
+
+        expect(response.data.length).toBeGreaterThan(0);
+    });
+
+    it('should return all tasks with completed attr = true', async ()=>{
+        const response = await axios.get(api_url+'/tasks?completed=1');
+
+        expect(response.data.length).toBeGreaterThan(0);
+    });
+
+    it('should return all tasks with completed attr = not', async ()=>{
+        expect(axios.get(api_url+'/tasks?completed=not')).rejects.toThrow('Request failed with status code 400');
+    });
+
+});
